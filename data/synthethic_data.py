a90c92adf4015e39f0cd12f90e0218b35d80d582
Emanuel Pacheco <emanuel.dias.pacheco@gmail.com>
2024-11-30 18:41:10 +0000

    Added data

---
+++ data/synthethic_data.py
@@ -0,0 +1,63 @@
+import numpy as np
+import pandas as pd
+
+candidates = ["Emanuel", "Nuno", "Filipa", "Maria", "Marta", "Branco", "Nulo"]
+
+prob_candidates = [0.4, 0.15, 0.1, 0.1, 0.05, 0.1, 0.1]
+
+districts = [
+    "Aveiro",
+    "Beja",
+    "Braga",
+    "Bragança",
+    "Castelo Branco",
+    "Coimbra",
+    "Évora",
+    "Faro",
+    "Guarda",
+    "Leiria",
+    "Lisboa",
+    "Portalegre",
+    "Porto",
+    "Santarém",
+    "Setúbal",
+    "Viana do Castelo",
+    "Vila Real",
+    "Viseu"
+]
+
+prob_districts = [
+    0.07,
+    0.02,
+    0.09,
+    0.03,
+    0.03,
+    0.04,
+    0.03,
+    0.03,
+    0.01,
+    0.04,
+    0.2,
+    0.03,
+    0.17,
+    0.04,
+    0.07,
+    0.03,
+    0.03,
+    0.04
+]
+
+total_districts = np.random.choice(districts, 100000, p=prob_districts)
+total_candidates = np.random.choice(candidates, 100000, p=prob_candidates)
+
+df = pd.DataFrame({
+    "district": total_districts,
+    "candidate": total_candidates
+})
+
+# save the pivot table to a csv file
+df.to_csv("votes.csv")
+
+#statistics
+print(df["candidate"].value_counts())
+print(df["district"].value_counts())
