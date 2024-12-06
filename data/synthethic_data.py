a8f22db06bc414012c93c8b67c950eea5e5fc153
MrBrodinha <95493968+MrBrodinha@users.noreply.github.com>
2024-12-06 12:32:25 +0000

    Some changes were made

--- data/synthethic_data.py
+++ data/synthethic_data.py
@@ -3,55 +3,17 @@ import pandas as pd
 
 candidates = ["Emanuel", "Nuno", "Filipa", "Maria", "Marta", "Branco", "Nulo"]
 
-prob_candidates = [0.4, 0.15, 0.1, 0.1, 0.05, 0.1, 0.1]
+prob_candidates = [0.4, 0.2, 0.1, 0.1, 0.1, 0.05, 0.05]
 
-districts = [
-    "Aveiro",
-    "Beja",
-    "Braga",
-    "Bragança",
-    "Castelo Branco",
-    "Coimbra",
-    "Évora",
-    "Faro",
-    "Guarda",
-    "Leiria",
-    "Lisboa",
-    "Portalegre",
-    "Porto",
-    "Santarém",
-    "Setúbal",
-    "Viana do Castelo",
-    "Vila Real",
-    "Viseu"
-]
+# randomize candidates order with a defined seed
+np.random.shuffle(candidates)
 
-prob_districts = [
-    0.07,
-    0.02,
-    0.09,
-    0.03,
-    0.03,
-    0.04,
-    0.03,
-    0.03,
-    0.01,
-    0.04,
-    0.2,
-    0.03,
-    0.17,
-    0.04,
-    0.07,
-    0.03,
-    0.03,
-    0.04
-]
+# randomize districts order with a defined seed
+np.random.shuffle(prob_candidates)
 
-total_districts = np.random.choice(districts, 1000, p=prob_districts)
 total_candidates = np.random.choice(candidates, 1000, p=prob_candidates)
 
 df = pd.DataFrame({
-    "Districts": total_districts,
     "Candidates": total_candidates
 })
 
