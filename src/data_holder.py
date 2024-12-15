56bdb4244d1bee457ddb9c5127e389d95677ff5e
MrBrodinha <95493968+MrBrodinha@users.noreply.github.com>
2024-12-15 18:43:28 +0000

    Prettier Prints

--- src/data_holder.py
+++ src/data_holder.py
@@ -1,7 +1,8 @@
 import tenseal as ts
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
-from .utils import read_data, write_data, write_data_append
+from .utils import read_data, write_data, write_data_append, print_section
+import sys
 
 class AbstractDataHandler:
     def __init__(self, context_file):
@@ -31,7 +32,7 @@ class DataHolder(AbstractDataHandler):
         Initialize the DataHolder by creating a context and loading data.
         """
         self.context = self.initialize_context()
-        self.data = self.load_data()
+        self.data = self.load_data("data/votes.csv")
 
     def initialize_context(self):
         """
@@ -51,22 +52,24 @@ class DataHolder(AbstractDataHandler):
         # Save the secret and public contexts
         self.__secret_context__ = context.serialize(save_secret_key=True)
         self.export_context(self.__secret_context__, "keys/secret.txt")
+        print("- Full Context (with Secret Key) saved to: keys/secret.txt")
 
         context.make_context_public()
         self.__public_context__ = context.serialize()
         self.export_context(self.__public_context__, "keys/public.txt")
+        print("- Public Context (without Secret Key) saved to: keys/public.txt")
 
         print("Context generated successfully!")
         return context
 
-    def load_data(self):
+    def load_data(self, path):
         """
         Load the dataset from a CSV file.
 
         Returns:
         - pd.DataFrame: The loaded dataset.
         """
-        return pd.read_csv("data/votes.csv")
+        return pd.read_csv(path)
 
     def export_context(self, context, filename):
         """
@@ -77,7 +80,6 @@ class DataHolder(AbstractDataHandler):
         - filename (str): Path to save the context.
         """
         write_data(filename, context)
-        print(f"Context exported successfully to {filename}!")
 
     def encrypt_data(self):
         """
@@ -93,10 +95,25 @@ class DataHolder(AbstractDataHandler):
         # Encrypt and save each candidate vector
         for i, candidato in enumerate(candidates):
             candidatos_enc = ts.ckks_vector(self.context, candidato)
+
+            # Calculate progress
+            progress = (i + 1) / 1000
+            length = int(10 * progress)
+            bar = "#" * length + "-" * (10 - length)
+
+            # Print the loading bar
+            sys.stdout.write(f"\r[{bar}] {int(progress * 100)}%")
+            sys.stdout.flush()
+
+            # Creates file with the encrypted data of first candidate
             if i == 0:
                 write_data("outputs/candidatos_enc.txt", candidatos_enc.serialize())
+
+            # Appends the encrypted data of the other candidates    
             else:
                 write_data_append("outputs/candidatos_enc.txt", candidatos_enc.serialize())
+
+        print()
 
     def decrypt_vector(self):
         """
@@ -105,7 +122,7 @@ class DataHolder(AbstractDataHandler):
         Returns:
         - list[float]: The decrypted vector.
         """
-        m_proto = read_data("outputs/soma.txt")
+        m_proto = read_data("outputs/sum_enc.txt")
         m = ts.lazy_ckks_vector_from(m_proto)
         m.link_context(self.context)
         return m.decrypt()
@@ -118,6 +135,13 @@ class DataHolder(AbstractDataHandler):
         candidates_mapping = dict(enumerate(self.encoder.categories_[0]))
         number_of_votes = self.decrypt_vector()
 
-        # Print the result for each candidate
+        # Print header
+        print("\n" + "-" * 30)
+        print("Candidate Results")
+        print("-" * 30)
+
+        # Print each candidate's name and votes
         for number, votes in enumerate(number_of_votes):
-            print(f"{candidates_mapping[number]}: {votes:.0f}")
+            candidate = candidates_mapping[number]
+            print(f"{candidate:<15} | {votes:>5.0f}")
+        print("-" * 30 + "\n")
